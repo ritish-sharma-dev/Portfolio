4600c58717f14edfc5957e6af31ab6e46bc3a73a
Ritish Sharma <ritishsharma04022006@gmail.com>
2026-06-11 12:06:29 +0530

    Backend Changes

--- server/server.js
+++ server/server.js
@@ -18,3 +18,5 @@ if (process.env.NODE_ENV !== "production"){
     const PORT=process.env.PORT || 5000;
     app.listen(PORT, ()=> console.log(`Server Is Running On Port: ${PORT}`));
 }
+
+export default app;
